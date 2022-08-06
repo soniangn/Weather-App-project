dc0dd2a5746e0c22bd970567f10afc3199b439f8
soniangn <nguyen.sonia@gmail.com>
2022-08-06 17:38:08 +0200

    Date and time set

---
+++ src/index.js
@@ -0,0 +1,25 @@
+//Display Date and Time
+let now = new Date();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let hours = now.getHours();
+let minutes = now.getMinutes();
+
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let date = document.querySelector("#date-time");
+date.innerHTML = `${day} ${hours}:${minutes}`;
