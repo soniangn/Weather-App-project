977e39d5fcc4998b83007219516be5776efa6e10
soniangn <nguyen.sonia@gmail.com>
2022-08-07 14:15:11 +0200

    Set the display weather of a searched city

--- src/index.js
+++ src/index.js
@@ -23,3 +23,42 @@ if (minutes < 10) {
 
 let date = document.querySelector("#date-time");
 date.innerHTML = `${day} ${hours}:${minutes}`;
+
+//Display temperature of searched city
+
+function displayTemperature(response) {
+  let temperature = response.data.main.temp;
+  let h1Element = document.querySelector("#temperature");
+  h1Element.innerHTML = Math.round(temperature);
+
+  let cityElement = document.querySelector("#searched-city");
+  cityElement.innerHTML = response.data.name;
+  console.log(response);
+
+  let skyElement = document.querySelector("#sky-description");
+  skyElement.innerHTML = response.data.weather[0].description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+function searchCity(city) {
+  let apiKey = "cf8b5baa824d961b33e0cb48813022bf";
+  let unit = "metric";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+  axios.get(url).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let inputCity = document.querySelector("#search-bar");
+  searchCity(inputCity.value);
+}
+
+let newCity = document.querySelector("#search-box");
+newCity.addEventListener("submit", handleSubmit);
+
+searchCity("Paris");
